cd528c7673a3f698ba72620a339c3860b7674cb3
Francis Solangon <127672085+francissolangon@users.noreply.github.com>
2025-06-17 20:33:07 +0800

    Add files via upload

---
+++ LabExer6A.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.labexer6a;
+
+/**
+ *
+ * @author Francis
+ */
+public class LabExer6A {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
